f54ef396afd742f4ee20f1d954fd2796d7ade388
Mikem <Mikemangan00@gmail.com>
2017-03-13 17:56:02 -0400

    started adding a listview. needs further work

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,12 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +15,30 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //create a ArrayList to store all the numbers in layout
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        words.add("Four");
+        words.add("Five");
+        words.add("Six");
+        words.add("Seven");
+        words.add("Eight");
+        words.add("Nine");
+        words.add("Ten");
+
+
+        //create an ArrayAdpter to hand the data off the ListView
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+
+        // find the parent element we want to insert Textviews into
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // set the adapter for the listView to use
+        listView.setAdapter(itemsAdapter);
+
+
     }
 }
